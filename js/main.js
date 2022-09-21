ba894aa1a1261c6ca8b4cfb784fce1dc21e16be1
Pedro Xavier <pedroxxavier@hotmail.com>
2022-09-21 14:24:07 -0300

    armazenando dados na web

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista")
+const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    const nome = evento.target.elements['nome'];
+    const quantidade =  evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
 })
 
 function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
 
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
 
-    lista.appendChild(novoItem)
+    lista.appendChild(novoItem);
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(itens));
 }
